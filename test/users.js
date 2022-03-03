2ecca4316cdeb3fb995a342e33c95cb58e1cda57
Gurvinder Singh <29sidhuz@gmail.com>
2022-03-03 08:20:25 -0600

    asserted new user in list

--- test/users.js
+++ test/users.js
@@ -2,6 +2,7 @@ let request = require('supertest');
 let { expect, assert } = require('chai');
 const baseURL = "jsonplaceholder.typicode.com";
 request = request(baseURL);
+let jpath = require('jsonpath');
 
 
 describe('users API', function () {
@@ -59,6 +60,28 @@ describe('users API', function () {
             then(function (res) {   // get the user id of first user and make second call to update his email
                 user_id = res.body[0].id;
                 return request.put(`/users/${user_id}`).send({ 'email': 'sampleEmail@jsonplaceholder.com' }).expect(200);
+            });
+    });
+
+    it('POST-create new user verify new user in GET all users list', function () {
+
+        const data = { 'name': 'sample', 'username': 'sample', email: 'sample@sample.com' };
+        return request.post('/users').
+            set('Content-type', 'application/json; charset=UTF-8').
+            send(data). // send the payload in the body of the request
+            expect(201). // assert if the response code is 201 - that is resource is created
+            then(function(res){
+                console.log(res.body);
+                // in response, API is sending the new user info which contains id and that is unique for each user
+                let new_id = res.body.id;
+                return request.get('/users').then(function(res){ //make GET all users API CALL
+                    all_users_json = res.body;  
+                    // console.log(JSON.stringify(all_users_json));
+                    all_users_id = jpath.query(all_users_json,'$.*.id'); // Response is json, so we can use jsonpath to get array of all user id
+                    // console.log(all_users_id);
+                    // expect(all_users_id).to.include(new_id); // assert if new user id exists/is included in all_users_id array. Fails if id doesn't exist
+                    assert.isNotOk(all_users_id.includes(new_id),"given user id does exist"); // asserts that result should be false (i.e. id should not be in the list as this is fake user creation)
+                });                
             });
     });
 });
